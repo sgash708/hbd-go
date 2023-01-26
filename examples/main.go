7c9a9fd42fc170e4d33aa82a47960242cd579e72
sgash708 <sugash7081023@gmail.com>
2023-01-26 13:33:24 +0900

    fix: ga

--- examples/main.go
+++ examples/main.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/sgash708/hbd-go/age"
+	age "github.com/sgash708/hbd-go"
 )
 
 func main() {
-
 	birthDay := time.Date(2020, 11, 1, 0, 0, 0, 0, time.Local)
 	targetDay := time.Date(2022, 1, 1, 0, 0, 0, 0, time.Local)
 	myAge := age.CalAge(birthDay, targetDay)
